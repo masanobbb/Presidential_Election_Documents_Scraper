b753714643df87579a6e56128db5e8ea9a83604c
masanobbb <mk.bko56@gmail.com>
2017-12-23 15:00:52 +0900

    deleted get_doc_type method and add the function into __get_documents_urls

--- pedspy.py
+++ pedspy.py
@@ -4,113 +4,103 @@ import time
 import pandas as pd
 from dateutil import parser
 
+class Pedspy(object):
 
-def get_documents_urls(candidate_name, year):
-    """
+    def __init__(self, candidate_name, year):
+        self.name = candidate_name
+        self.year = year
+        self.doc_type_urls = self.__get_documents_urls()
+        self.documents = self.__get_documents()
 
-    :param candidate_name: president candidate name
-    :param year:        the year of election
-    :return:            the list of the urls where there are the candidate's campaign speeches, statements, press releases, so on.
-    """
-    root_url = 'http://www.presidency.ucsb.edu/'
-    target_url = root_url + str(year) + '_election.php'
+    def __get_documents_urls(self):
+        """
 
-    response = request.urlopen(target_url)
-    body = response.read()
-    soup = BeautifulSoup(body, "html.parser")
+        :param candidate_name: president candidate name
+        :param year:        the year of election
+        :return:            the dictionary of the documents' type and url where many docs exists
+                            such as the candidate's campaign speeches, statements, press releases, so on.
+        """
+        root_url = 'http://www.presidency.ucsb.edu/'
+        target_url = root_url + str(self.year) + '_election.php'
 
-    # find urls where any kind of the president candidate's documents are sleeping
-    urls = []
-    for a in soup.find_all('a'):
-        last_name = candidate_name.split()[-1]
-        if last_name in a.get('href').lower():
-            # print(a.get('href'))
-            urls.append(root_url + a.get('href'))
+        response = request.urlopen(target_url)
+        body = response.read()
+        soup = BeautifulSoup(body, "html.parser")
 
-    return urls
+        # find urls where any kind of the president candidate's documents are sleeping
+        doc_urls = {}
+        for a in soup.find_all('a'):
+            last_name = self.name.split()[-1]
+            if last_name in a.get('href').lower():
+                doc_type = a.get_text().replace(' ','_')
+                url = root_url + a.get('href')
+                doc_urls[doc_type] = url
 
+        return doc_urls
 
-def get_document_type(url):
-    """
+    def __get_documents(self):
+        """
 
-    :param url: the url has a type of documents such as campaign speeches, statements, press releases, so on.
-    :return: the document type of presidential election documents
-    """
-    doc_type = ""
+        :param candidate_name:  president candidate name
+        :param year:            the year of election
+        :return:                the list of the urls where there are the candidate's campaign speeches, statements, press releases, so on.
+        """
+        documents_df = pd.DataFrame()
+        root_url = "http://www.presidency.ucsb.edu"
+        count = 0
 
-    return doc_type
+        # get the documents' text data
+        for doc_type, url in self.doc_type_urls.items():
 
+            response = request.urlopen(url)
+            body = response.read*()
+            soup = BeautifulSoup(body, "html,parser")
 
-def get_documents(year, candidate_name):
-    """
+            for link in soup.find_all('a'):
 
-    :param candidate_name:  president candidate name
-    :param year:            the year of election
-    :return:                the list of the urls where there are the candidate's campaign speeches, statements, press releases, so on.
-    """
-    documents_df = pd.DataFrame()
-    root_url = "http://www.presidency.ucsb.edu"
-    count = 0
+                href = link.get('href')
 
-    # get the urls where "campaign speeches", "statements", "press releases", etc. are sleeping
-    documentsUrls = get_documents_urls(year, candidate_name)
+                if 'index.php' in href and 'ws' in href:
+                    href = href.replace('..', root_url)
 
-    # get the documents' text data
-    for url in documentsUrls:
+                    # keep the stating time
+                    startTime = time.time()
 
-        response = request.urlopen(url)
-        body = response.read*()
-        soup = BeautifulSoup(body, "html,parser")
+                    page_response = request.urlopen(href)
+                    page_body = page_response.read()
+                    soup = BeautifulSoup(page_body, "html.parser")
 
-        # get the type of documents
-        # such as "campaign speeches", "statements", "press releases", etc.
-        doctype = get_document_type()
+                    speech = ""
+                    title = ""
+                    date = ""
+                    text = ""
+                    source_url = href
 
-        for link in soup.find_all('a'):
+                    for display_text in soup.find_all("span", class_="displaytext"):
+                        text = text + display_text.text
+                        break
 
-            href = link.get('href')
+                    for papers_title in soup.find_all("span", class_="paperstitle"):
+                        title = papers_title.text
+                        break
 
-            if 'index.php' in href and 'ws' in href:
-                href = href.replace('..', root_url)
+                    for doc_date in soup.find_all("span", class_="docdate"):
+                        date = parser.parse(doc_date.text).strftime('%Y-%m-%d')
+                        break
 
-                # keep the stating time
-                startTime = time.time()
+                    documents_df = pd.concat([documents_df,
+                                          pd.DataFrame({"date": date,
+                                                        "doctype": doctype,
+                                                        "title": title,
+                                                        "speech": speech,
+                                                        "source": source_url},
+                                                       index=[count])])
 
-                page_response = request.urlopen(href)
-                page_body = page_response.read()
-                soup = BeautifulSoup(page_body, "html.parser")
+                    # take some time to get next documents
+                    count += 1
+                    end_time = time.time()
+                    if (end_time - startTime) > 1:
+                        time.sleep(1)
 
-                speech = ""
-                title = ""
-                date = ""
-                text = ""
-                source_url = href
-
-                for display_text in soup.find_all("span", class_="displaytext"):
-                    text = text + display_text.text
-                    break
-
-                for papers_title in soup.find_all("span", class_="paperstitle"):
-                    title = papers_title.text
-                    break
-
-                for doc_date in soup.find_all("span", class_="docdate"):
-                    date = parser.parse(doc_date.text).strftime('%Y-%m-%d')
-                    break
-
-                documents_df = pd.concat([documents_df,
-                                      pd.DataFrame({"date": date,
-                                                    "doctype": doctype,
-                                                    "title": title,
-                                                    "speech": speech,
-                                                    "source": source_url},
-                                                   index=[count])])
-
-                # take some time to get next documents
-                count += 1
-                end_time = time.time()
-                if (end_time - startTime) > 1:
-                    time.sleep(1)
-
-        return documents_df
+            return documents_df
 
